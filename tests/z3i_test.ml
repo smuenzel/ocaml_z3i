5a18c8c455750e62c996aa81c6257248f822bd1e
Stefan Muenzel <source@s.muenzel.net>
2022-05-31 18:47:29 +0800

    remove commented code

--- tests/z3i_test.ml
+++ tests/z3i_test.ml
@@ -15,34 +15,3 @@ let%expect_test "popcount" =
   |> [%sexp_of: Model.t Solver_result.t]
   |> print_s;
   [%expect {| Unsatisfiable |}]
-
-(* {[
-     let%expect_test "popcount" =
-       let open Z3i in
-       let c = Context.create () in
-       let s = Solver.create c in
-       let length = 64 in
-       let sort = Sort.create_bitvector c ~bits:length in
-       let value = Expr.const_i 0 sort in
-       let popcount2 =
-         Bitvector.extract
-           ~high:6
-           ~low:0
-           (Bitvector.neg
-              (List.reduce_balanced_exn ~f:Bitvector.add
-                 (List.init 64
-                    ~f:(fun i ->
-                        Bitvector.rotate_left_const value i
-                      )
-                 )
-              ))
-       in
-       [ Boolean.neq (Bitvector.popcount ~result_bit_size:7 value) popcount2
-       ]
-       |> Solver.add_list s;
-       Solver.check_current_and_get_model s
-       |> [%sexp_of: Model.t Solver_result.t]
-       |> print_s;
-       [%expect {| Unsatisfiable |}]
-   ]}
-*)
